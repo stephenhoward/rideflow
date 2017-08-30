3927ba3e18b097981d637ef67014c1740c7a8fe4
Stephen Howard <stephen@goldenguru.com>
2017-08-30 09:36:38 -0700

    fix up generated code, add manage vehicle endpoint

---
+++ lib/RideFlow/API/Controller/Vehicle.pm
@@ -0,0 +1,7 @@
+package RideFlow::API::Controller::Vehicle;
+
+use Mojo::Base 'RideFlow::API::Controller';
+
+has model => 'Vehicle';
+
+1;
--- lib/RideFlow/Model.pm
+++ lib/RideFlow/Model.pm
@@ -3,6 +3,7 @@ package RideFlow::Model;
 use Moose;
 
 use RideFlow::Model::Route;
+use RideFlow::Model::Vehicle;
 
 sub m {
 
